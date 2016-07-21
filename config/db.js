6bf1ceab425358c91e1921baa89a6501fe3f732c
Sun <orcfeng@gmail.com>
2016-07-21 21:34:53 +0000

    update db schema

--- config/db.js
+++ config/db.js
@@ -2,55 +2,7 @@ var mongoose = require( 'mongoose' );
 var Hash = require('password-hash');
 var Schema   = mongoose.Schema;
 
-//users
-//var User = new Schema({
-//	uid : String,
-//	username : String,
-//	openid : String,
-//	email : String,
-//	password : String,
-//	role : Number,
-//	bio : String,
-//	tags : String
-//	
-//});
 
-
-var Follow = new Schema({
-	follower : Schema.Types.ObjectId,
-	followho : Schema.Types.ObjectId
-});
-
-var Favorite = new Schema({
-	uid : Schema.Types.ObjectId,
-	caseid : Schema.Types.ObjectId
-});
-
-// applications
-var OCase = new Schema({
-    subject    : String,
-    description : String,
-    tags       : String,
-    startdate  : Date,
-    endate     : Date,
-    status     : Number,
-    ctype      : Number,
-    isprivate  : Boolean,
-    currentStep : Number,
-    summary    : String,
-    parentid   : Schema.Types.ObjectId,
-    uid        : Schema.Types.ObjectId
-
-});
- 
-var Step = new Schema({
-	index : Number,
-	step : String,
-	note : String,
-	caseid : Schema.Types.ObjectId,
-	createdate : Date,
-	subcase : Schema.Types.ObjectId
-});
 
 var Todo = new Schema({
 	   todo:String,
@@ -61,7 +13,7 @@ var Todo = new Schema({
 	   status:Number
 });
 
-var Comment = new Schema({
+var Checklist = new Schema({
 	    comment:String,
 		ctime:Date,
 		status:Number,
@@ -69,49 +21,20 @@ var Comment = new Schema({
 		caseid:Schema.Types.ObjectId
 });
 
-var Tag = new Schema({
-	tag : String,
-	count : Number,
-	note : String
-});
 
-var Question = new Schema({
+
+var Event = new Schema({
 	question:String,
 	description:String,
 	
 });
 
-var Answer = new Schema({
-	
-});
-
-
-//admin
-var Notification = new Schema({
-	
-});
-
-var Message = new Schema({
-	
-});
-
-var Trace = new Schema({
-	
-});
 
 //mongoose.model( 'User', User );
-mongoose.model( 'Follow', Follow );
-mongoose.model( 'Favorite', Favorite );
-mongoose.model( 'OCase', OCase );
-mongoose.model( 'Step', Step );
-mongoose.model( 'Todo', Todo );
-mongoose.model( 'Comment', Comment );
-mongoose.model( 'Tag', Tag );
-mongoose.model( 'Question', Question );
-mongoose.model( 'Answer', Answer );
-mongoose.model( 'Notification', Notification );
-mongoose.model( 'Message', Message );
-mongoose.model( 'Trace', Trace );
+mongoose.model( 'todo', Todo );
+mongoose.model( 'checklist', Checklist );
+mongoose.model( 'event', Event );
+
 
 var host = process.env.OPENSHIFT_MONGODB_DB_HOST||"localhost";
 var port = process.env.OPENSHIFT_MONGODB_DB_PORT||'27017';
